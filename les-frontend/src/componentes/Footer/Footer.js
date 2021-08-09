6cb37369405e397116dba3d0750051f38fef63a5
Rafael Mio <rafamioads@gmail.com>
2021-08-09 14:45:47 -0300

    Footer

---
+++ les-frontend/src/componentes/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./style.css";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer class="bg-light p-5 border mt-5">
+        <p class="text-secondary text-center">Todos os direitos reservados</p>
+      </footer>
+    );
+  }
+}
--- les-frontend/src/rotas.js
+++ les-frontend/src/rotas.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Navbar from "./componentes/Navbar/Navbar";
+import Footer from "./componentes/Footer/Footer";
 
 export default class Rotas extends Component {
   render() {
     return (
       <BrowserRouter>
         <Navbar />
-        <Route></Route>
+        <Route>
+
+        </Route>
+        <Footer />
       </BrowserRouter>
     );
   }
