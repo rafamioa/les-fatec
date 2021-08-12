41b53cb165a884931cf682886fa291ce425e3a6a
Kprado <kaiqueprado13@hotmail.com>
2021-08-12 01:28:14 -0300

    Init  API backend

---
+++ Back-end/API-Rest-JavaSpring/src/main/java/br/gov/sp/fatec/APIRestJavaSpring/ApiRestJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.APIRestJavaSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestJavaSpringApplication.class, args);
+	}
+
+}
