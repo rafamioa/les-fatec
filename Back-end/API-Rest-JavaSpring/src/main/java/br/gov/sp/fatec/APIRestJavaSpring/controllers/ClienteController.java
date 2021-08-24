3cc60554a4407a7aeb6a401dbe3bad1cdf7f942b
Rafael Mio <rafamioads@gmail.com>
2021-08-24 20:42:10 -0300

    Configuração do cors - permitir requisições do frontend

--- Back-end/API-Rest-JavaSpring/src/main/java/br/gov/sp/fatec/APIRestJavaSpring/controllers/ClienteController.java
+++ Back-end/API-Rest-JavaSpring/src/main/java/br/gov/sp/fatec/APIRestJavaSpring/controllers/ClienteController.java
@@ -1,15 +1,21 @@
 package br.gov.sp.fatec.APIRestJavaSpring.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.gov.sp.fatec.APIRestJavaSpring.modelos.Cliente;
 
 @RestController
 @RequestMapping(path = "/cliente")
 public class ClienteController {
 	
-	@GetMapping
-	public String cadastrar() {
-		return "cadastrado";
+	@PostMapping
+	public ResponseEntity<Cliente> cadastrar(@RequestBody Cliente cliente) {
+		System.out.println("cliente =>>>> " + cliente.toString());
+		return ResponseEntity.ok().body(cliente);
 	}
 }
--- Back-end/API-Rest-JavaSpring/src/main/java/br/gov/sp/fatec/APIRestJavaSpring/modelos/Cliente.java
+++ Back-end/API-Rest-JavaSpring/src/main/java/br/gov/sp/fatec/APIRestJavaSpring/modelos/Cliente.java
@@ -1,6 +1,6 @@
 package br.gov.sp.fatec.APIRestJavaSpring.modelos;
 
-import java.util.Date;
+import java.io.Serializable;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,38 +8,39 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity(name="clientes")
-
-public class Cliente {
+public class Cliente implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
-	private Date dataDeNascimento;
+//	private Date dataDeNascimento;
 	private String cpf;
 	private String genero;
 	private String telefone;
 	private String email;
 	private String senha;
 	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
+//	public Long getId() {
+//		return id;
+//	}
+//	public void setId(Long id) {
+//		this.id = id;
+//	}
 	public String getNome() {
 		return nome;
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public Date getDataDeNascimento() {
-		return dataDeNascimento;
-	}
-	public void setDataDeNascimento(Date dataDeNascimento) {
-		this.dataDeNascimento = dataDeNascimento;
-	}
+//	public Date getDataDeNascimento() {
+//		return dataDeNascimento;
+//	}
+//	public void setDataDeNascimento(Date dataDeNascimento) {
+//		this.dataDeNascimento = dataDeNascimento;
+//	}
 	public String getCpf() {
 		return cpf;
 	}
